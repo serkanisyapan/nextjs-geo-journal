3f52701cd4a09136dd8505399fd345e9bf7cf346
serkanisyapan <serkanisyapan1@hotmail.com>
2023-03-01 19:29:37 +0300

    setup eslint and prettier

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string
+  name: string;
 }
 
 export default function handler(
